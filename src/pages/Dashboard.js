d312461dd27fea477541f922ca074ca2496ff981
romgarofficial <romgarofficial@gmail.com>
2023-08-03 12:37:45 +0800

    fixed null null null

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,7 @@
 import {Nav, Navbar, Container, Button, Image} from "react-bootstrap"
-import { NavLink } from "react-router-dom";
+import { NavLink, Navigate } from "react-router-dom";
 import { useEffect } from "react";
+import  secureLocalStorage  from  "react-secure-storage";
 
 import Logo from "../images/logo.png"
 import Banner from "../components/Banner.js"
@@ -11,11 +12,20 @@ export default function Dashboard(){
     })
     return(
         <>
+        {
+            secureLocalStorage.getItem("id") === null ?
+            <>
+            <Navigate to={"/logout"}/>
+            </>
+            :
+            <>
             <Container fluid className="rounded vh-75 bg-lightgray text-center d-flex justify-content-center align-items-center flex-column">
                 <h1 className="display-2 fw-bold">DASHBOARD</h1>
                 <p>DASHBOARD</p>
                 <Button variant="warning" as={NavLink} to={"/"} className="px-5">Home</Button>
             </Container>
+        </>
+        }
         </>
     )
 }
