9a02fbd34b974d36c292772957c9b20bac2967e2
romgarofficial <romgarofficial@gmail.com>
2023-08-06 16:39:57 +0800

    edited app.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -12,11 +12,17 @@ export default function Dashboard(){
     })
     return(
             <>
-            <Container fluid className="rounded vh-75 bg-lightgray text-center d-flex justify-content-center align-items-center flex-column">
+            {
+                secureLocalStorage.getItem("id") === null || secureLocalStorage.getItem("id") === undefined ?
+                <Navigate to={"/"}/>
+                :
+                <Container fluid className="rounded vh-75 bg-lightgray text-center d-flex justify-content-center align-items-center flex-column">
                 <h1 className="display-2 fw-bold">DASHBOARD</h1>
                 <p>DASHBOARD</p>
                 <Button variant="warning" as={NavLink} to={"/"} className="px-5">Home</Button>
-            </Container>
+                </Container>
+            }
+           
         </>
     )
 }
